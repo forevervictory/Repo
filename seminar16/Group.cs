20194cbe92f5f676875141ef1c3618a3d39383a9
forevervictory <forevervictory@bk.ru>
2016-01-11 16:04:08 +0300

    Entity

---
+++ seminar16/Group.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace seminar16
+{
+    class Group
+    {
+        public int Id { get; set; }
+        public int Number { get; set; }
+        public List<Student> Students { get; set; }
+
+    }
+}
